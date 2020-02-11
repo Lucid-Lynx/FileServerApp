adb8d4e1bc193884b71d9db74092e615ec32ca54
Kirill Kanin <lvs97a@gmail.com>
2020-02-11 21:34:34 +0300

    +

--- server/role_model_sql.py
+++ server/role_model_sql.py
@@ -49,6 +49,39 @@ class RoleModelSQL:
 
             """
 
-            pass
+            request = args[1]
+            session_id = request.headers.get('Authorization')
 
-        pass
+            if not session_id:
+                raise web.HTTPUnauthorized(text='Unauthorized request')
+
+            with closing(psycopg2.connect(**conn_params)) as conn:
+                with conn.cursor(cursor_factory=DictCursor) as cursor:
+                    cursor.execute(sql.SQL(
+                        'SELECT * FROM public."Session" AS S JOIN public."User" AS U '
+                        'ON S."user_id" = U."Id" WHERE "UUID" = {}').format(sql.Literal(session_id)))
+                    session = cursor.fetchone()
+
+                    if not session:
+                        raise web.HTTPUnauthorized(text='Session expired. Please, sign in again')
+
+                    if not session.get('role_id'):
+                        raise web.HTTPForbidden(text='User is not attached to role')
+
+                    cursor.execute(sql.SQL('SELECT * FROM public."Method" WHERE "Name" = {}').format(
+                        sql.Literal(func.__name__)))
+                    method = cursor.fetchone()
+
+                    if method and not method['Shared']:
+                        cursor.execute(sql.SQL(
+                            'SELECT * FROM public."Role" AS R JOIN public."MethodRole" AS MR '
+                            'ON R."Id" = MR."role_id" WHERE R."Id" = {} AND MR."method_id" = {}').format(
+                                sql.Literal(session['role_id']), sql.Literal(method['Id'])))
+                        relation = cursor.fetchone()
+
+                        if not relation:
+                            raise web.HTTPForbidden(text='Access denied')
+
+                return func(*args, **kwargs)
+
+        return wrapper
