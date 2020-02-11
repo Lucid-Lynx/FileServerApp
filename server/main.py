dd56d9ebe18036443521741629998ebd938aec5b
Kirill Kanin <lvs97a@gmail.com>
2020-02-11 21:28:05 +0300

    +

--- server/main.py
+++ server/main.py
@@ -22,6 +22,85 @@ def commandline_parser() -> argparse.ArgumentParser:
     pass
 
 
+def get_file_data(path):
+    """Get full info about file.
+
+    Args:
+        path (str): Working directory path.
+
+    Returns:
+        Dict, which contains full info about file. Keys:
+            name (str): name of file with .txt extension.
+            content (str): file content.
+            create_date (str): date of file creation.
+            edit_date (str): date of last file modification.
+            size (int): size of file in bytes.
+
+    Raises:
+        AssertionError: if file does not exist, filename format is invalid,
+        ValueError: if security level is invalid.
+
+    """
+
+    pass
+
+
+def create_file(path):
+    """Create new .txt file.
+
+    Method generates name of file from random string with digits and latin letters.
+
+    Args:
+        path (str): Working directory path.
+
+    Returns:
+        Dict, which contains name of created file. Keys:
+            name (str): name of file with .txt extension.
+            content (str): file content.
+            create_date (str): date of file creation.
+            size (int): size of file in bytes,
+            user_id (int): user Id.
+
+    Raises:
+        AssertionError: if user_id is not set,
+        ValueError: if security level is invalid.
+
+    """
+
+    pass
+
+
+def delete_file(path):
+    """Delete file.
+
+    Args:
+        path (str): Working directory path.
+
+    Returns:
+        Str with filename with .txt file extension.
+
+    Raises:
+        AssertionError: if file does not exist.
+
+    """
+
+    pass
+
+
+def change_dir(path):
+    """Change working directory.
+
+    Args:
+        path (str): Working directory path.
+
+    Returns:
+        Str with successfully result.
+
+    """
+
+    pass
+
+
 def main():
     """Entry point of app.
 
