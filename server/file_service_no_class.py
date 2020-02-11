fd17f3fc4e4d2770aaeca126ba94ab800aeba0ce
Kirill Kanin <lvs97a@gmail.com>
2020-02-11 21:26:18 +0300

    +

---
+++ server/file_service_no_class.py
@@ -0,0 +1,101 @@
+# Copyright 2019 by Kirill Kanin.
+# All rights reserved.
+
+
+import os
+import sys
+import server.utils as utils
+
+extension = 'txt'
+
+
+def change_dir(path):
+    """Change current directory of app.
+
+    Args:
+        path (str): Path to working directory with files.
+
+    Raises:
+        AssertionError: if directory does not exist.
+
+    """
+
+    pass
+
+
+def get_file_data(filename):
+    """Get full info about file.
+
+    Args:
+        filename (str): Filename without .txt file extension.
+
+    Returns:
+        Dict, which contains full info about file. Keys:
+            name (str): name of file with .txt extension.
+            content (str): file content.
+            create_date (str): date of file creation.
+            edit_date (str): date of last file modification.
+            size (int): size of file in bytes.
+
+    Raises:
+        AssertionError: if file does not exist, filename format is invalid.
+
+    """
+
+    pass
+
+
+def get_files():
+    """Get info about all files in working directory.
+
+    Returns:
+        List of dicts, which contains info about each file. Keys:
+            name (str): name of file with .txt extension.
+            create_date (str): date of file creation.
+            edit_date (str): date of last file modification.
+            size (str): size of file in bytes.
+
+    """
+
+    pass
+
+
+def create_file(content=None):
+    """Create new .txt file.
+
+    Method generates name of file from random string with digits and latin letters.
+
+    Args:
+        content (str): String with file content.
+
+    Returns:
+        Dict, which contains name of created file. Keys:
+            name (str): name of file with .txt extension.
+            content (str): file content.
+            create_date (str): date of file creation.
+            size (int): size of file in bytes,
+            user_id (int): user Id.
+
+    Raises:
+        AssertionError: if user_id is not set.
+
+    """
+
+    pass
+
+
+def delete_file(filename):
+    """Delete file.
+
+    Args:
+        filename (str): Filename without .txt file extension.
+
+    Returns:
+        Str with filename with .txt file extension.
+
+    Raises:
+        AssertionError: if file does not exist.
+
+    """
+
+    pass
