a5079dc373e146f64b413b5ae7f229945bd82fa8
Kirill Kanin <lvs97a@gmail.com>
2020-02-11 21:38:38 +0300

    +

--- server/main.py
+++ server/main.py
@@ -158,12 +158,17 @@ def main():
     Command line options:
     -p --port - port (default: 8080).
     -f --folder - working directory (absolute or relative path, default: current app folder FileServer).
+    -i --init - initialize database.
     -h --help - help.
 
     """
 
     parser = commandline_parser()
     namespace = parser.parse_args(sys.argv[1:])
+
+    db = DataBase()
+    if namespace.init:
+        db.init_system()
 
     handler = Handler(namespace.folder)
     app = web.Application()
@@ -173,9 +178,19 @@ def main():
         web.get('/files', handler.get_file_info),
         web.post('/files', handler.create_file),
         web.delete('/files/{filename}', handler.delete_file),
+        web.get('/files/download', handler.download_file),
+        web.get('/files/download/queued', handler.download_file_queued),
         web.post('/signup', handler.signup),
         web.post('/signin', handler.signin),
         web.get('/logout', handler.logout),
+        web.put('/method/{method_name}', handler.add_method),
+        web.delete('/method/{method_name}', handler.delete_method),
+        web.put('/role/{role_name}', handler.add_role),
+        web.delete('/role/{role_name}', handler.delete_role),
+        web.post('/add_method_to_role', handler.add_method_to_role),
+        web.post('/delete_method_from_role', handler.delete_method_from_role),
+        web.post('/change_shared_prop', handler.change_shared_prop),
+        web.post('/change_user_role', handler.change_user_role),
         web.post('/change_file_dir', handler.change_file_dir),
     ])
     logging.basicConfig(level=logging.INFO)
